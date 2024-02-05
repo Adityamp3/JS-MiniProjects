485d5c06d55b9144ac828edd399c2a79683dfab0
Adityamp3 <dealwithaditya@gmail.com>
2024-02-05 20:35:50 +0530

    BE

--- BackEnd/script.js
+++ BackEnd/script.js
@@ -14,3 +14,14 @@ console.log("*******************************************************************
 // To install any npm package, just open terminal and type "npm i <package_name>"
 var oneLinerJoke = require('one-liner-joke');
 console.log(oneLinerJoke.getRandomJoke());
+
+// Another npm package 
+var figlet = require("figlet");
+figlet("Aditya Raj Lodhi", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
